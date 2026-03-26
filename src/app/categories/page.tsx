86455c70d152e1636e13e64626dbbd31f78f44f8
Waldemar Tschaikowski <waldemar80tschaikowski@gmail.com>
2026-03-26 11:04:28 +0100

    feat: initial commit

--- src/app/categories/page.tsx
+++ src/app/categories/page.tsx
@@ -1,19 +1,37 @@
 import Category from "@/types/Category";
 import Link from "next/link";
+import Image from "next/image";
 
 export default async function Categories() {
   const res = await fetch("https://api.escuelajs.co/api/v1/categories");
   const categories: Category[] = await res.json();
 
   const categoriesList = (
-    <ul>
+    <ul className="flex flex-wrap gap-5 items-center justify-center">
       {categories.map((c) => (
         <li key={c.id}>
-          <Link href={`categories/${c.id}`}>{c.name}</Link>
+          <div className="bg-white rounded-2xl shadow-2xl p-2 w-58 flex flex-col gap-2 items-center">
+            <Link href={`categories/${c.id}`}>
+              <h3>{c.name}</h3>
+              <Image
+                className="cursor-copy"
+                src={c.image}
+                width={200}
+                height={200}
+                alt={c.name}
+                unoptimized
+              />
+            </Link>
+          </div>
         </li>
       ))}
     </ul>
   );
 
-  return <div>{categoriesList}</div>;
+  return (
+    <section className="bg-blue-200 min-h-screen py-6 px-4 md:px-20">
+      <h2 className="text-center text-4xl text-white mb-8">Kategories</h2>
+      {categoriesList}
+    </section>
+  );
 }
